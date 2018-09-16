4c3cd639fa1b5baa0cf77ad3e4844af9948c3bc9
Gaurav C <cggaurav@gmail.com>
2018-09-16 16:06:45 -0500

    Progress.

--- scrape/scrape.js
+++ scrape/scrape.js
@@ -29,20 +29,97 @@ const KEYS_PAGE = [
 const CSV_KEYS = require('../data/keys.json')
 const ZONES = require('../data/zones.json')
 
+function cleanup (data) {
+
+    // HACK + UGLY
+    return data.body
+        .replace(/\r\n\s+/g, '|')
+        .replace(/;\s+/g, '|')
+        // .replace(/ - /g, '|')
+        .replace('International|Coastal Cleanup, Singapore', 'ICCS Singapore')
+        .replace('DEBRIS SUMMARY|', '')
+        .replace('CLEANUP SUMMARY|', '')
+        .replace('DEBRIS SUMMARY (PIE CHART)|', '')
+        .replace('TOP 10 ITEMS at a Glance|', '')
+        .replace('Total Distance|(metres)|', 'Total distance in meters|')
+        .replace('Total number of|participants', 'Total number of participants')
+        .replace('Total number of|trash bags filled', 'Total number of trash bags filled')
+        .replace('Weight of trash|bags collected (kg)', 'Weight of trash bags collected (kg)')
+        .replace('Total distance|(metres)', 'Weight of trash bags collected (kg)')
+        .replace('Food Wrappers (candy, chips,|etc)','Food Wrappers (candy chips etc)')
+        .replace('Fishing line ( 1 meter = 1|piece)','Fishing line ( 1 meter = 1 piece )')
+        .replace('Appliances (fridges, washers,|etc)', 'Appliances (fridges, washers, etc)')
+        .replace('Items of Local|Concern', 'Items of Local Concern')
+
+        // .replace('Most Likely to Find Items|', '')
+        // .replace('Fishing Gear||', '')
+        // .replace('Packaging|Materials', '')
+        // .replace('Other trash|', '')
+        // .replace('Items of Local Concern|', '')
+        // .replace('Personal Hygiene|', '')
+        .split('|')
+}
+
+function year(body, index, csv) {
+    if (body[index].startsWith('2015')) {
+      csv['Year'] = '2015'
+    }
+
+    if (body[index].startsWith('2016')) {
+      csv['Year'] = '2016'
+    }
+
+    if (body[index].startsWith('2017')) {
+      csv['Year'] = '2017'
+    }
+
+    if (body[index].startsWith('2018')) {
+      csv['Year'] = '2018'
+    }
+
+    return csv
+}
+
 osmosis
 .get(EXAMPLES[0])
+// .get('http://coastalcleanup.nus.edu.sg/results/2017/index.html')
+// .follow('li a@href')
 .set({
-	// FILTER LATER
+	// FILTER LATER, SUCKS
     'body': 'body'
 })
-.data((listing) => {
+.then((document, data, next) => {
+
     // CLEANUP
-    let body = listing.body.replace(/\r\n\s+/g, '|').replace(/;\s+/g, '|').replace(/ - /g, '|').replace(/\|/g, "\n")
+    let body = cleanup(data)
 
-    console.log("AFTER", body)
+    let csv = Object.assign({}, CSV_KEYS)
 
-    // TODO: Do a custom map
+    console.log(JSON.stringify(body))
+
+    // STEP THROUGH
+    body.forEach((element, index) => {
+        try {
+            element = element.trim()
+
+            // YEAR
+            csv = year(body, index, csv)
+            csv['URL'] = document.location.href
+
+
+        } catch (e) {
+
+        }
+    })
+
+    console.log("CSV")
+    console.log(csv)
+
+    next(document, data)
 })
-.log(console.log)
+.data(() => {})
+.log((data) => {
+    console.log(data)
+})
 // .error(console.log)
 // .debug(console.log)
